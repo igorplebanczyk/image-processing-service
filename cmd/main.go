81e674d923c316f7408640a43793b9df6701af45
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-11-06 21:38:20 +0100

    refactor: refactor cmd/main.go

--- cmd/main.go
+++ cmd/main.go
@@ -14,47 +14,58 @@ import (
 	"time"
 )
 
+const envPath string = "../.env"
+
 func main() {
-	err := godotenv.Load("../.env")
+	err := godotenv.Load(envPath)
 	if err != nil {
 		slog.Error("Error loading .env file")
 		return
 	}
 
+	port := os.Getenv("PORT")
+	dbURL := os.Getenv("DATABASE_URL")
+	jwtSecret := os.Getenv("JWT_SECRET")
+	azureStorageAccountName := os.Getenv("AZURE_STORAGE_ACCOUNT_NAME")
+	azureStorageAccountKey := os.Getenv("AZURE_STORAGE_ACCOUNT_KEY")
+	azureStorageAccountURL := os.Getenv("AZURE_STORAGE_ACCOUNT_URL")
+	azureStorageContainerName := os.Getenv("AZURE_STORAGE_CONTAINER_NAME")
+
 	dbService := database.NewService()
-	err = dbService.Connect(os.Getenv("DATABASE_URL"))
-	if err != nil {
-		slog.Error("Error connecting to database", "error", err)
-		return
-	}
 
 	userRepo := database.NewUserRepository(dbService.DB)
 	refreshTokenRepo := database.NewRefreshTokenRepository(dbService.DB)
 	imageRepo := database.NewImageRepository(dbService.DB)
 
-	authService := auth.NewService(userRepo, refreshTokenRepo, os.Getenv("JWT_SECRET"), 15*time.Minute, 15*time.Hour)
-	usersCfg := users.NewConfig(userRepo, refreshTokenRepo)
-
+	authService := auth.NewService(userRepo, refreshTokenRepo, jwtSecret, 15*time.Minute, 15*time.Hour)
 	storageService, err := storage.NewService(
-		os.Getenv("AZURE_STORAGE_ACCOUNT_NAME"),
-		os.Getenv("AZURE_STORAGE_ACCOUNT_KEY"),
-		os.Getenv("AZURE_STORAGE_ACCOUNT_URL"),
-		os.Getenv("AZURE_STORAGE_CONTAINER_NAME"),
+		azureStorageAccountName,
+		azureStorageAccountKey,
+		azureStorageAccountURL,
+		azureStorageContainerName,
 	)
 	if err != nil {
 		slog.Error("Error creating storage service", "error", err)
 		return
 	}
 
+	usersCfg := users.NewConfig(userRepo, refreshTokenRepo)
 	imagesCfg := images.NewConfig(imageRepo, storageService)
 
-	port, err := strconv.Atoi(os.Getenv("PORT"))
+	portInt, err := strconv.Atoi(port)
 	if err != nil {
 		slog.Error("Error parsing port", "error", err)
 		return
 	}
 
-	serverService := server.NewService(port, dbService, authService, usersCfg, imagesCfg)
+	serverService := server.NewService(portInt, dbService, authService, usersCfg, imagesCfg)
+
+	err = dbService.Connect(dbURL)
+	if err != nil {
+		slog.Error("Error connecting to database", "error", err)
+		return
+	}
+
 	err = serverService.StartServer()
 	if err != nil {
 		slog.Error("Error starting server", "error", err)
