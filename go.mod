45e6e0374b252a21dfde80dc231218670add3fae
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-11-07 15:10:49 +0100

    feat: add the cache service implementation

--- go.mod
+++ go.mod
@@ -13,6 +13,9 @@ require (
 	github.com/Azure/azure-sdk-for-go/sdk/azcore v1.14.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/internal v1.10.0 // indirect
 	github.com/Azure/azure-sdk-for-go/sdk/storage/azblob v1.4.1 // indirect
+	github.com/cespare/xxhash/v2 v2.1.2 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/go-redis/redis/v8 v8.11.5 // indirect
 	github.com/lib/pq v1.10.9 // indirect
 	golang.org/x/net v0.27.0 // indirect
 	golang.org/x/text v0.19.0 // indirect
---
+++ internal/services/cache/cache.go
@@ -0,0 +1,61 @@
+package cache
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"github.com/go-redis/redis/v8"
+	"time"
+)
+
+type Service struct {
+	client *redis.Client
+	ctx    context.Context
+}
+
+func NewService(addr string, password string) (*Service, error) {
+	client := redis.NewClient(&redis.Options{
+		Addr:     addr,
+		Password: password,
+		DB:       0,
+	})
+
+	if err := client.Ping(context.Background()).Err(); err != nil {
+		return nil, fmt.Errorf("failed to connect to redis: %w", err)
+	}
+
+	return &Service{
+		client: client,
+		ctx:    context.Background(),
+	}, nil
+}
+
+func (s *Service) Set(key string, value any, expiration time.Duration) error {
+	err := s.client.Set(s.ctx, key, value, expiration).Err()
+	if err != nil {
+		return fmt.Errorf("failed to set key: %w", err)
+	}
+
+	return nil
+}
+
+func (s *Service) Get(key string) (any, error) {
+	val, err := s.client.Get(s.ctx, key).Result()
+	if err != nil {
+		if errors.Is(err, redis.Nil) {
+			return "", nil
+		}
+		return "", fmt.Errorf("failed to get value of key: %w", err)
+	}
+
+	return val, nil
+}
+
+func (s *Service) Delete(key string) error {
+	err := s.client.Del(s.ctx, key).Err()
+	if err != nil {
+		return fmt.Errorf("failed to delete key: %w", err)
+	}
+
+	return nil
+}
