f5edcf4fb0eacd2dfec168f3be40ada9afcfb1d2
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-11-15 19:00:27 +0100

    fix: fix file security issues

--- internal/common/log/log.go
+++ internal/common/log/log.go
@@ -13,16 +13,16 @@ func Setup(logDir string) error {
 
 	_, err := os.Stat(logDir)
 	if os.IsNotExist(err) {
-		err = os.MkdirAll(logDir, 0777)
+		err = os.MkdirAll(logDir, 0700)
 		if err != nil {
 			return fmt.Errorf("failed to create log directory: %w", err)
 		}
 	}
 
 	logFileName := currDate + ".log"
-	logFilePath := filepath.Join(logDir, logFileName)
+	logFilePath := filepath.Clean(filepath.Join(logDir, logFileName))
 
-	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0666)
+	file, err := os.OpenFile(logFilePath, os.O_CREATE|os.O_APPEND|os.O_RDWR, 0600)
 	if err != nil {
 		return fmt.Errorf("failed to open log file: %w", err)
 	}
