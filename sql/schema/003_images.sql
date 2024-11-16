4e37727a71f7747064e3e58c98cedc76df7bdfd7
igorplebanczyk <plebanczyk.igor@gmail.com>
2024-11-16 11:46:51 +0100

    fix: fix image names not being unique within the scope of a user

--- sql/schema/003_images.sql
+++ sql/schema/003_images.sql
@@ -2,6 +2,7 @@ CREATE TABLE IF NOT EXISTS images (
     id UUID PRIMARY KEY,
     user_id UUID REFERENCES users(id) ON DELETE CASCADE,
     name VARCHAR(255) NOT NULL,
-    created_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP NOT NULL,
+    CONSTRAINT unique_name_per_user UNIQUE (user_id, name)
 );
